552aa0699d22fac0e31cfc1d372d55c6843db587
Clifford Hunter <clifford.hunter@mykenzie.snhu.edu>
2023-07-11 09:54:40 -0400

    trying to correct backend error

--- packages/client/src/configs/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = process.env.VITE_API_URL
-// any other environment variables or other constants you wish to use
--- packages/client/src/util/axiosConfig.js
+++ packages/client/src/util/axiosConfig.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_NODE_ENV === "production" 
-  ? import.meta.env.VITE_API_URL 
-  : "http://localhost:3001/api"
-});
+// // const api = axios.create({
+// //   baseURL: import.meta.env.VITE_NODE_ENV === "production" 
+// //   ? import.meta.env.VITE_API_URL 
+// //   : "http://localhost:3001/api"
+// // });
 
-export const setAuthHeaders = (token) => {
-  if (!token) delete api.defaults.headers.common["Authorization"];
+// // export const setAuthHeaders = (token) => {
+// //   if (!token) delete api.defaults.headers.common["Authorization"];
 
-  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-};
+// //   api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+// // };
 
-export default api;
+// // export default api;
