e0e7b1f4d524b8365db747708f881a8308dded7d
Eliot Slavik <115732920+EliotSlavik@users.noreply.github.com>
2023-07-13 10:34:04 -0500

    cors fixed

--- packages/server/app.js
+++ packages/server/app.js
@@ -14,7 +14,12 @@ mongoose
   .catch((err) => console.error(err));
 
 const app = express();
-app.use(cors());
+// added to fix CORS errors
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use(keys.app.apiUrl, router);
 // add the following
