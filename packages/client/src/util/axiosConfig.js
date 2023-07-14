f0f496607a893dcd9789a819252a7572970c466b
Karl-Knapp <Karl-Knapp@outlook.com>
2023-07-13 22:45:42 -0500

    Task stuff

--- packages/client/src/util/axiosConfig.js
+++ packages/client/src/util/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_NODE_ENV === "development" ? process.env.REACT_APP_API_DOMAIN : `${process.env.REACT_APP_API_DOMAIN}${process.env.REACT_APP_API_URL}`,
+  baseURL: process.env.REACT_APP_NODE_ENV === "development" ? `${process.env.REACT_APP_API_DOMAIN}${process.env.REACT_APP_API_URL}` : process.env.REACT_APP_API_DOMAIN,
   withCredentials: true,
 });
 
